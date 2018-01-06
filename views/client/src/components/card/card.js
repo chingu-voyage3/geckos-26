0463e0ed3e03694b75bc060c65a510c42cecd9d2
Fabien SHAN <shan.fabien@gmail.com>
2018-01-07 00:49:26 +0900

    Pasted client folder from master

---
+++ views/client/src/components/card/card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './card.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="card">
+        <span>First Card!!</span>
+      </div>
+    );
+  }
+}
+
+export default Card;
