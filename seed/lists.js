eda8b0ef72993eecfb18a5cfef26ea93ca2b6e57
Fabien SHAN <shan.fabien@gmail.com>
2018-01-12 01:09:40 +0900

    Modified list seeding with dummy cards and comments

--- seed/lists.js
+++ seed/lists.js
@@ -1,19 +1,39 @@
 var mongoose = require("mongoose");
 var List = require("../models/list");
+var Card = require("../models/card");
+var Comment = require("../models/comment");
 
 var data = [
   {
-    title: 'item 1'
+    title: 'item 1',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porta enim sit amet pharetra euismod. Quisque mattis leo condimentum semper feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas rutrum erat vulputate neque mattis vulputate. Integer non elit vitae nibh tincidunt pretium. Sed dignissim gravida ullamcorper. Mauris sagittis justo a nibh rhoncus, vitae viverra dolor luctus.',
   },
   {
-    title: 'item 1'
+    title: 'item 2',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porta enim sit amet pharetra euismod. Quisque mattis leo condimentum semper feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas rutrum erat vulputate neque mattis vulputate. Integer non elit vitae nibh tincidunt pretium. Sed dignissim gravida ullamcorper. Mauris sagittis justo a nibh rhoncus, vitae viverra dolor luctus.',
   },
   {
-    title: 'item 1'
+    title: 'item 3',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porta enim sit amet pharetra euismod. Quisque mattis leo condimentum semper feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas rutrum erat vulputate neque mattis vulputate. Integer non elit vitae nibh tincidunt pretium. Sed dignissim gravida ullamcorper. Mauris sagittis justo a nibh rhoncus, vitae viverra dolor luctus.',
   },
 ];
 
 function seedDB(){
+
+  Card.remove({}, function(err){
+    if(err){
+      console.log(err);
+    }
+    console.log("Cards removed");
+  });
+
+  Comment.remove({}, function(err){
+    if(err){
+      console.log(err);
+    }
+    console.log("Comments removed");
+  });
+
   // Remove all lists
   List.remove({}, function(err){
     if(err){
@@ -29,11 +49,47 @@ function seedDB(){
             console.log(err);
           } else {
             console.log("Added list!");
+            // Add a card in the card
+            for (var i = 0; i < 2; i++) {
+              Card.create(
+                {
+                    title: "This is a great note",
+                    description: "This is truly a great description of this great note",
+                    author: "Fabien"
+                }, function(err, card){
+                  if(err){
+                    console.log(err);
+                  } else {
+                    // Add a comment in the list
+                    Comment.create(
+                      {
+                        text: "This is a great comment",
+                        author: "ThisIsMe"
+                      },
+                      function(err, comment){
+                        if(err){
+                          console.log(err);
+                        } else {
+                          // Add a comment in the card
+                          card.comments.push(comment);
+                          card.save()
+                        }
+                      }
+                    )
+
+                    list.cards.push(card);
+                    list.save();
+                    console.log("Created a new card");
+                  }
+                }
+              );
+            }
           }
         });
       });
     }
   });
+
 }
 
 module.exports = seedDB;
