df0840245797da65e3a51f4de30ce32901e0b0eb
Archit Kaushik <asarchit65@gmail.com>
2018-01-13 13:46:11 +0530

    WIP : modals

--- views/client/src/components/card/card.js
+++ views/client/src/components/card/card.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './card.css';
+import CardModal from './CardModal';
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  
   render() {
     return (
-      <div className="card">
+      <div className="card" onClick={this.toggleModal}>
         <span>{ this.props.card.title }</span>
+        <CardModal show={this.state.isOpen}
+        onClose={this.toggleModal}>
+        `Here's some content for the modal`
+        </CardModal>
       </div>
+      
     );
   }
 }
