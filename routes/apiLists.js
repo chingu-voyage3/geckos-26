44665e2bee36ee94a764f7d7be5f430d24abed1a
Fabien SHAN <shan.fabien@gmail.com>
2018-01-17 23:26:16 +0900

    Modified fetch POST route to api/list (RESTful)

--- routes/apiLists.js
+++ routes/apiLists.js
@@ -2,10 +2,24 @@ var express = require('express');
 var router = express.Router();
 var List = require('../models/list');
 
+// Comment seedDB
 var seedDB = require('../seed/lists');
 seedDB();
 
-// get a list of lists from the db
+
+// =====================
+//   List - CRUD routes
+// =====================
+
+// CREATE list route
+router.post("/list", function(req, res, next){
+  List.create(req.body).then(function(list){
+    res.send(list);
+  }).catch(next);
+});
+
+
+// READ list route
 router.get('/list', function(req, res, next){
   List.find({})
     .populate(
@@ -19,6 +33,29 @@ router.get('/list', function(req, res, next){
     .then(function(list){
       res.send(list);
     });
+});
+
+// UPDATE list route
+router.put('/list/:id', function(req, res, next){
+  List.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+    List.findOne({_id: req.params.id}).then(function(list){
+      res.send(list);
+    });
+  });
+});
+
+// DELETE list route
+// Also delete related comments
+router.delete('/list/:id', function(req, res, next){
+  List.findOne({_id: req.params.id}).then(function(list){
+    list.cards.forEach(function(card){
+      Card.findByIdAndRemove(card).then(function(){
+        List.findByIdAndRemove({_id: req.params.id}).then(function(list){
+          res.send(list);
+        });
+      });
+    });
+  });
 });
 
 module.exports = router;
